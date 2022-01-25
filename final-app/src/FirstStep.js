13c0afc8ac534705aedeb30a2b016c77d75253c0
Abhilash Mishra <abhilashmishra548@gmail.com>
2022-01-25 16:35:28 +0530

    firststep completed

--- final-app/src/FirstStep.js
+++ final-app/src/FirstStep.js
@@ -6,7 +6,7 @@ import * as Yup from "yup";
 
 function FirstStep(){
 
-    const {setCurrentStep} = useContext(globalData)
+    const {setCurrentStep,setFinalData,finalData} = useContext(globalData)
 
     const initialValues = {
         "fname":"",
@@ -15,15 +15,19 @@ function FirstStep(){
         "phoneNumbers":[""]
         
     };
+
     const validationSchema = Yup.object({
         "fname":Yup.string().required("Can't Left Empty !").max(8,"Reached Max Limit !").min(5,"Minimum 5 Characters Are Required"),
         "lname":Yup.string().required("Can't Left Empty !").max(8,"Reached Max Limit !").min(2,"Minimum 2 Characters Are Required"),
         "email":Yup.string().required("Can't Left Empty !").email("Enter Valid Email").matches(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,"invalid email format")
-    })
+    });
+
     const onSubmit = values=>{
+        setFinalData({...finalData,values})
         console.log(values);
+        setCurrentStep(2);
         //setFormData({values})
-    }
+    };
 
     return(
         <React.Fragment>
@@ -49,8 +53,9 @@ function FirstStep(){
                                                         type="email"
                                                         name="email"
                                                         label="Email"></HelperController>
-                                        <label htmlFor="Phone Numbers">Phone Numbers</label>
-                                        
+
+
+                                        {/* <label htmlFor="Phone Numbers">Phone Numbers</label>                                        
                                         <FieldArray name="phoneNumbers">
                                             {
                                                 (obj)=>{
@@ -75,11 +80,43 @@ function FirstStep(){
                                                     </div>
                                                 }
                                             }
+                                        </FieldArray> */}
+
+                                        
+                                        <label htmlFor="Phone Numbers">Phone Numbers</label>                                        
+                                        <FieldArray name="phoneNumbers">
+                                            {
+                                                (my_obj)=>{
+                                                    const { form,push,remove } = my_obj;
+                                                    const { values } = form;
+                                                    const { phoneNumbers } = values;
+                                                    return <div>
+                                                        {
+                                                            phoneNumbers.map((element,index)=>(
+                                                                <div key={index}>
+                                                                    {/* <Field name={`phoneNumbers[${index}]`} type="text" className="array-styles"></Field> */}
+                                                                    <HelperController name={`phoneNumbers[${index}]`} control="input"></HelperController>
+                                                                    {
+                                                                        index>0?(<button onClick={()=>remove(index)} 
+                                                                                className="btn btn-danger btn-sm">-</button>):null
+                                                                    }
+                                                                    {
+                                                                        index>0?null:<button onClick={()=>push("")} className="btn btn-success btn-sm">+</button>
+                                                                    }
+                                                                </div>
+                                                            ))
+                                                        }
+                                                    </div>
+                                                }
+                                            }
                                         </FieldArray>
 
-                                            <br></br><br></br>
 
-                                        <button onClick={()=>setCurrentStep(2)} disabled={!formik.isValid}>Next</button>
+
+
+                                            <br></br><br></br>
+                                            {/* onClick={()=>setCurrentStep(2)} */}
+                                        <button disabled={!formik.isValid} type="submit">Next</button>
                                     </Form>
                                 )
                             }
